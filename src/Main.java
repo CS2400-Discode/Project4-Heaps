0481544f2f254fba95b94992ed46eaccff197421
Owen Lovett <olovett214@gmail.com>
2022-04-14 17:11:49 -0700

    started main.java and moved txt files
    for the files to be read, it has to be in main project folder.
    
    need to find a way to print from heap and print number of swaps

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+       ArrayList readFile = new ArrayList();
 
+        Scanner file1 = new Scanner(new File("data_random.txt"));
+
+        while (file1.hasNextInt()) {            //reads data in file to ArrayList
+            readFile.add(file1.nextInt());
+        }
+
+        Integer[] data = new Integer[readFile.size()];
+
+        for (int i = 0; i < readFile.size(); i++){          //copies contents from Arraylist to normal array
+            data[i] = (int) readFile.get(i);
+        }
+
+        MaxHeap heap1 = new MaxHeap();
+        heap1.sequentialMaxHeap(data);
     }
 }
