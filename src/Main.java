a859245a3f3841a06f4d736fcdf95b6a54642c1f
Nil Patel <89549847+nilrnp@users.noreply.github.com>
2022-04-16 16:09:13 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,27 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         ArrayList readFile = new ArrayList();
+        Scanner file1 = new Scanner(new File("data_sorted.txt"));
 
-        Scanner file1 = new Scanner(new File("data_random.txt"));
-
-        while (file1.hasNextInt()) {            //reads data in file to ArrayList
+        while (file1.hasNextInt()) {
             readFile.add(file1.nextInt());
         }
 
         Integer[] data = new Integer[readFile.size()];
-
-        for (int i = 0; i < readFile.size(); i++) {          //copies contents from Arraylist to normal array
+        for (int i = 0; i < readFile.size(); i++) {
             data[i] = (int) readFile.get(i);
         }
 
         MaxHeap heap1 = new MaxHeap(data.length);
+
         heap1.sequentialMaxHeap(data);
+        heap1.getFirst10();
+        System.out.println(heap1.getNumOfSwaps());
+
+        MaxHeap heap2 = new MaxHeap(data.length);
+        heap2.optimalMaxHeap(data);
+        heap2.getFirst10();
+        System.out.println(heap2.getNumOfSwaps());
 
 //        try {
 //            FileWriter writer = new FileWriter("output.txt");
