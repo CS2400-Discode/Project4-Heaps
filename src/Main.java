6aa9b7dd680eb530fb6a100037738ed0afcab443
Nil Patel <nilrnp@gmail.com>
2022-04-15 21:53:33 -0700

    messed around with main class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
-import java.io.FileNotFoundException;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
-       ArrayList readFile = new ArrayList();
+    public static void main(String[] args) throws IOException {
+        ArrayList readFile = new ArrayList();
 
         Scanner file1 = new Scanner(new File("data_random.txt"));
 
@@ -16,11 +17,26 @@ public class Main {
 
         Integer[] data = new Integer[readFile.size()];
 
-        for (int i = 0; i < readFile.size(); i++){          //copies contents from Arraylist to normal array
+        for (int i = 0; i < readFile.size(); i++) {          //copies contents from Arraylist to normal array
             data[i] = (int) readFile.get(i);
         }
 
-        MaxHeap heap1 = new MaxHeap();
+        MaxHeap heap1 = new MaxHeap(data.length);
         heap1.sequentialMaxHeap(data);
+
+//        try {
+//            FileWriter writer = new FileWriter("output.txt");
+//            writer.write("Heap built using sequential insertions: \n");
+//            writer.write("Number of swaps in the heap creation: \n");
+//            writer.write("Heap after 10 removals: \n");
+//            writer.write("\n");
+//            writer.write("Heap built using sequential insertions: \n");
+//            writer.write("Number of swaps in the heap creation: \n");
+//            writer.write("Heap after 10 removals: \n");
+//        }
+//        catch (IOException e)
+//        {
+//            e.printStackTrace();
+//        }
     }
 }
--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 public final class MaxHeap<T extends Comparable<? super T>> implements MaxHeapInterface<T>
 {
 	private T[] heap;
