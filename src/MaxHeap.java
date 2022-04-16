3a834095d67f96ec4be063ff50f3448387194e18
Nil Patel <89549847+nilrnp@users.noreply.github.com>
2022-04-16 16:08:24 -0700

    Update MaxHeap.java

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,167 +1,189 @@
-public final class MaxHeap<T extends Comparable<? super T>> implements MaxHeapInterface<T>
+import java.util.Arrays;
+
+public final class MaxHeap<T extends Comparable<? super T>>
+             implements MaxHeapInterface<T>
 {
-	private T[] heap;
-	private int lastIndex;
-	private int swapIndex;
-	private boolean initialized = false;
-	private static final int DEFAULT_CAPACITY = 25;
-	private static final int MAX_CAPACITY = 10000;
+   private T[] heap;
+   private int lastIndex;
+   private boolean initialized = false;
+   private static final int DEFAULT_CAPACITY = 25;
+   private static final int MAX_CAPACITY = 10000;
+   private static int numOfSwaps = 0;
+   
+   public MaxHeap()
+   {
+      this(DEFAULT_CAPACITY);
+   }
+   
+   public MaxHeap(int initialCapacity)
+   {
+      if (initialCapacity < DEFAULT_CAPACITY)
+         initialCapacity = DEFAULT_CAPACITY;
+      else
+         checkCapacity(initialCapacity);
 
-	public MaxHeap() {
-		this(DEFAULT_CAPACITY);
-	}
+      @SuppressWarnings("unchecked")
+      T[] tempHeap = (T[])new Comparable[initialCapacity + 1];
+      heap = tempHeap;
+      lastIndex = 0;
+      initialized = true;
+   }
 
-	public MaxHeap(int initialCapacity) {
-		if (initialCapacity < DEFAULT_CAPACITY)
-			initialCapacity = DEFAULT_CAPACITY;
-		else
-			checkCapacity(initialCapacity);
+   public T getMax()
+   {
+      checkInitialization();
+      T root = null;
+      if (!isEmpty())
+         root = heap[1];
+      return root;
+   }
 
-		@SuppressWarnings("unchecked")
-		T[] tempHeap = (T[]) new Comparable[initialCapacity + 1];
-		heap = tempHeap;
-		lastIndex = 0;
-		initialized = true;
-	}
+   public boolean isEmpty()
+   {
+      return lastIndex < 1;
+   }
 
-	public void sequentialMaxHeap(T[] entries) {
-		new MaxHeap(entries.length);
-		checkInitialization();
-		for (int index = 0; index < entries.length; index++)
-		{
-			add(entries[index]);
-			reheap(index + 1);
-		}
-	}
+   public int getSize()
+   {
+      return lastIndex;
+   }
 
-	public void optimalMaxHeap(T[] entries) {
-		new MaxHeap(entries.length);
-		checkInitialization();
-		for (int index = 0; index < entries.length; index++)
-			add(entries[index]);
+   public void clear()
+   {
+      checkInitialization();
+      while (lastIndex > -1)
+      {
+         heap[lastIndex] = null;
+         lastIndex--;
+      }
+      lastIndex = 0;
+   }
 
-		for (int rootIndex = lastIndex / 2; rootIndex > 0; rootIndex--)
-			reheap(rootIndex);
-	}
+   public void add(T newEntry)
+   {
+      checkInitialization();
+      int newIndex = lastIndex + 1;
+      int parentIndex = newIndex / 2;
+      while ((parentIndex > 0) && newEntry.compareTo(heap[parentIndex]) > 0)
+      {
+         heap[newIndex] = heap[parentIndex];
+         newIndex = parentIndex;
+         parentIndex = newIndex / 2;
+      }
+      heap[newIndex] = newEntry;
+      lastIndex++;
+   }
 
+   public T removeMax()
+   {
+      checkInitialization();
+      T root = null;
+      if (!isEmpty())
+      {
+         root = heap[1];
+         heap[1] = heap[lastIndex];
+         lastIndex--;
+         reheap(1);
+      }
+      return root;
+   }
 
-	public T getMax() {
-		checkInitialization();
-		T root = null;
-		if (!isEmpty())
-			root = heap[1];
-		return root;
-	}
+   private void reheap(int rootIndex) {
+      boolean done = false;
+      T orphan = heap[rootIndex];
+      int leftChildIndex = 2 * rootIndex;
 
-	public boolean isEmpty() {
-		return lastIndex < 1;
-	}
+      while (!done && (leftChildIndex <= lastIndex)) {
+         int largerChildIndex = leftChildIndex;
+         int rightChildIndex = leftChildIndex + 1;
 
-	public int getSize() {
-		return lastIndex;
-	}
+         if ((rightChildIndex <= lastIndex) && heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
+         {
+            largerChildIndex = rightChildIndex;
+         }
 
-	public void clear() {
-		checkInitialization();
-		while (lastIndex > -1)
-		{
-			heap[lastIndex] = null;
-			lastIndex--;
-		}
-		lastIndex = 0;
-	}
+         if (orphan.compareTo(heap[largerChildIndex]) < 0)
+         {
+            heap[rootIndex] = heap[largerChildIndex];
+            rootIndex = largerChildIndex;
+            leftChildIndex = 2 * rootIndex;
+            numOfSwaps++;
+         }
+         else
+         {
+            done = true;
+         }
+      }
+      heap[rootIndex] = orphan;
+   }
 
-	public void add(T newEntry) {
-		checkInitialization();
-		int newIndex = lastIndex + 1;
-		int parentIndex = newIndex / 2;
-		while ((parentIndex > 0) && newEntry.compareTo(heap[parentIndex]) > 0)
-		{
-			heap[newIndex] = heap[parentIndex];
-			newIndex = parentIndex;
-			parentIndex = newIndex / 2;
-		}
-		heap[newIndex] = newEntry;
-		lastIndex++;
-		ensureCapacity();
-	}
+   public T remove(T anEntry) {
+      checkInitialization();
+      boolean found = false;
+      for (int index = 0; index < lastIndex; index++)
+      {
+         if (heap[index].equals(anEntry))
+         {
+            found = true;
+            break;
+         }
+      }
+      if (!found)
+         throw new IllegalStateException("Error: entry not found.");
 
-	private void ensureCapacity() {
-	}
+      for (int index = 0; index < lastIndex + 1; index++)
+         heap[index] = heap[index + 1];
+      lastIndex--;
+      if (!isEmpty())
+      {
+         for (int rootIndex = lastIndex / 2; rootIndex > 0; rootIndex--)
+            reheap(rootIndex);
+      }
+      return anEntry;
+   }
 
-	public T removeMax() {
-		checkInitialization();
-		T root = null;
-		if (!isEmpty())
-		{
-			root = heap[1];
-			heap[1] = heap[lastIndex];
-			lastIndex--;
-			reheap(1);
-		}
-		return root;
-	}
+   public void optimalMaxHeap(T[] entries) {
+      lastIndex = entries.length;
+      assert initialized = true;
 
-	public T remove(T anEntry) {
-		checkInitialization();
-		boolean found = false;
-		int index = 0;
-		for (; index < lastIndex; index++)
-		{
-			if (heap[index].equals(anEntry))
-			{
-				found = true;
-				break;
-			}
-		}
-		if (!found)
-			throw new IllegalStateException("Error: entry not found.");
-		for (; index < lastIndex + 1; index++)
-			heap[index] = heap[index + 1];
-		lastIndex--;
-		if (!isEmpty())
-		{
-			for (int rootIndex = lastIndex / 2; rootIndex > 0; rootIndex--)
-				reheap(rootIndex);
-		}
-		return anEntry;
-	}
+      for (int index = 0; index < entries.length; index++)
+         heap[index + 1] = entries[index];
 
-	private void reheap(int rootIndex) {
-		boolean done = false;
-		T orphan = heap[rootIndex];
-		int leftChildIndex = 2 * rootIndex;
+      for (int rootIndex = lastIndex / 2; rootIndex > 0; rootIndex--)
+         reheap(rootIndex);
+   }
 
-		while (!done && (leftChildIndex <= lastIndex)) {
-			int largerChildIndex = leftChildIndex;
-			int rightChildIndex = leftChildIndex + 1;
-			if ((rightChildIndex <= lastIndex) && heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
-			{
-				largerChildIndex = rightChildIndex;
-			}
-			if (orphan.compareTo(heap[largerChildIndex]) < 0)
-			{
-				heap[rootIndex] = heap[largerChildIndex];
-				rootIndex = largerChildIndex;
-				leftChildIndex = 2 * rootIndex;
-				swapIndex++;
-			}
-			else
-			{
-				done = true;
-			}
-		}
-		heap[rootIndex] = orphan;
-	}
-	private void checkCapacity(int capacity)
-	{
-		if (capacity > MAX_CAPACITY)
-			throw new IllegalStateException("Attempt to create a heap over the capacity of " + MAX_CAPACITY);
-	}
+   public void sequentialMaxHeap(T[] entries) {
 
-	private void checkInitialization()
-	{
-		if (!initialized)
-			throw new SecurityException("ArrayBag object is corrupt.");
-	}
+      assert initialized = true;
+      for (int index = 0; index < entries.length; index++)
+      {
+         add(entries[index]);
+      }
+      reheap(1);
+   }
+
+   private void checkCapacity(int initialCapacity) {
+      if (initialCapacity > MAX_CAPACITY)
+         throw new IllegalStateException("Attempt to create a heap over the capacity of " + MAX_CAPACITY);
+   }
+
+   private void checkInitialization() {
+      if (!initialized)
+         throw new SecurityException("ArrayBag object is corrupt.");
+   }
+
+   public int getNumOfSwaps(){
+      int swaps = numOfSwaps;
+      return swaps;
+   }
+
+   public void getFirst10()
+   {
+      for(int i=1; i <10; i ++)
+      {
+         System.out.print(heap[i] + ",");
+      }
+      System.out.print("...");
+   }
 }
